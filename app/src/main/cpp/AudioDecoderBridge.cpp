a516c9b0b69194dcfee55a635cd4dab7a0322819
guoxinggen <xinggen.guo@ushow.media>
2022-06-21 13:41:51 +0800

    增加解码中间文件

---
+++ app/src/main/cpp/AudioDecoderBridge.cpp
@@ -0,0 +1,52 @@
+//
+// Created by guoxinggen on 2022/6/21.
+//
+
+#include <jni.h>
+#include "musicdecoder/audio_decoder_controller.h"
+
+AudioDecoderController *audioDecoderController;
+
+extern "C"
+JNIEXPORT jboolean JNICALL
+Java_com_audio_study_ffmpegdecoder_audiotracke_AudioDecoderImpl_getMusicMeta(JNIEnv *env, jobject thiz, jstring music_path, jintArray meta_array) {
+    const char *audioPath = env->GetStringUTFChars(music_path, NULL);
+    jint *metaArray = env->GetIntArrayElements(meta_array, NULL);
+    AudioDecoderController *audioDecoderController = new AudioDecoderController();
+    int result = audioDecoderController->getMusicMeta(audioPath, metaArray);
+    delete audioDecoderController;
+    env->ReleaseIntArrayElements(meta_array, metaArray, NULL);
+    env->ReleaseStringUTFChars(music_path, audioPath);
+    return result == 0;
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_audio_study_ffmpegdecoder_audiotracke_AudioDecoderImpl_prepareDecoder(JNIEnv *env, jobject thiz, jstring music_path) {
+    const char *audioPath = env->GetStringUTFChars(music_path, NULL);
+    audioDecoderController = new AudioDecoderController();
+    audioDecoderController->prepare(audioPath);
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_audio_study_ffmpegdecoder_audiotracke_AudioDecoderImpl_closeFile(JNIEnv *env, jobject thiz) {
+    if (NULL != audioDecoderController) {
+        audioDecoderController->destroy();
+        delete audioDecoderController;
+        audioDecoderController = NULL;
+    }
+}
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_audio_study_ffmpegdecoder_audiotracke_AudioDecoderImpl_readSamples(JNIEnv *env, jobject thiz, jshortArray samples, jint size) {
+    if (NULL != audioDecoderController) {
+        short *samplesArray = env->GetShortArrayElements(samples, NULL);
+        int result = audioDecoderController->readSapmles(samplesArray, size);
+        env->ReleaseShortArrayElements(samples, samplesArray, 0);
+        return result;
+    }
+}
+
+
--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -419,52 +419,10 @@ Java_com_audio_study_ffmpegdecoder_MainActivity_playAudioTest(JNIEnv *env, jobje
 }
 
 
-
-AudioDecoderController *audioDecoderController;
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_audio_study_ffmpegdecoder_MainActivity_stopAudioTest(JNIEnv *env, jobject thiz) {
     stop = true;
 }
 
-extern "C"
-JNIEXPORT jboolean JNICALL
-Java_com_audio_study_ffmpegdecoder_audiotracke_AudioDecoderImpl_getMusicMeta(JNIEnv *env, jobject thiz, jstring music_path, jintArray meta_array) {
-    const char* audioPath = env -> GetStringUTFChars(music_path,NULL);
-    jint* metaArray = env -> GetIntArrayElements(meta_array,NULL);
-    AudioDecoderController *audioDecoderController = new AudioDecoderController();
-    int result = audioDecoderController->getMusicMeta(audioPath, metaArray);
-    delete audioDecoderController;
-    env->ReleaseIntArrayElements(meta_array, metaArray, NULL);
-    env->ReleaseStringUTFChars(music_path, audioPath);
-    return result == 0;
-}
 
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_audio_study_ffmpegdecoder_audiotracke_AudioDecoderImpl_prepareDecoder(JNIEnv *env, jobject thiz,jstring music_path) {
-    const char *audioPath = env->GetStringUTFChars(music_path, NULL);
-    audioDecoderController = new AudioDecoderController();
-    audioDecoderController->prepare(audioPath);
-}
-
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_audio_study_ffmpegdecoder_audiotracke_AudioDecoderImpl_closeFile(JNIEnv *env, jobject thiz) {
-    if(NULL != audioDecoderController) {
-        audioDecoderController->destroy();
-        delete audioDecoderController;
-        audioDecoderController = NULL;
-    }
-}
-
-extern "C"
-JNIEXPORT jint JNICALL
-Java_com_audio_study_ffmpegdecoder_audiotracke_AudioDecoderImpl_readSamples(JNIEnv *env, jobject thiz, jshortArray samples, jint size) {
-    if(NULL != audioDecoderController) {
-        short * samplesArray = env->GetShortArrayElements(samples,NULL);
-        int result = audioDecoderController->readSapmles(samplesArray, size);
-        env->ReleaseShortArrayElements(samples, samplesArray, 0);
-        return result;
-    }
-}
